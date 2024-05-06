32e7272a7f257e754e6c50687afb7d626c527b92
maria-paps <maria.paps114@gmail.com>
2024-05-06 13:16:52 +0300

    server

---
+++ public/js/fetch_home_page.js
@@ -0,0 +1,14 @@
+// Make a GET request to the server to fetch the home page
+fetch('http://localhost:3000/')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.text();
+  })
+  .then(html => {
+    document.body.innerHTML = html;
+  })
+  .catch(error => {
+    console.error('Error fetching home page:', error);
+  });
