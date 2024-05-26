6360f3508012ac1a93bfd47f6e46ce8b4ec5494c
maria-paps <maria.paps114@gmail.com>
2024-05-26 20:15:02 +0300

    report

--- model/student_services_lite.mjs
+++ model/student_services_lite.mjs
@@ -101,7 +101,7 @@ const updateSemester = (academic_id) => {
 const parseDate = (dateString) => {
     const parts = dateString.split('-');
     const day = parseInt(parts[0], 10);
-    const month = parseInt(parts[1], 10) - 1; // Month is zero-indexed
+    const month = parseInt(parts[1], 10) - 1; 
     const year = parseInt(parts[2], 10);
     return new Date(year, month, day);
 };
